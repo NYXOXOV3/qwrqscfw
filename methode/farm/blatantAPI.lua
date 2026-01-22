c834cad805535c5d0d32bfbf6f0be17cf128e58c
NYXOXOV3 <fortressjr25@gmail.com>
2026-01-23 04:07:19 +0700

    Create blatantAPI.lua

---
+++ methode/farm/blatantAPI.lua
@@ -0,0 +1,85 @@
+-- =========================================================
+-- AUTO FISHING BLATANT FUNCTION
+-- =========================================================
+
+local BlatantAPI = {}
+
+-- ================= SERVICES =================
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- ================= CONTROLLER =================
+local FC = require(
+    ReplicatedStorage:WaitForChild("Controllers").FishingController
+)
+
+-- ================= REMOTES =================
+local Net = ReplicatedStorage
+    .Packages._Index["sleitnick_net@0.2.0"].net
+
+local RF_Charge   = Net["RF/ChargeFishingRod"]
+local RF_Start    = Net["RF/RequestFishingMinigameStarted"]
+local RE_Complete = Net["RE/FishingCompleted"]
+local RE_Equip    = Net["RE/EquipToolFromHotbar"]
+local RF_Cancel   = Net["RF/CancelFishingInputs"]
+local RF_Update   = Net["RF/UpdateAutoFishingState"]
+
+-- ================= CONFIG =================
+BlatantAPI.Config = {
+    Active = false,
+    Mode = "Old",
+    CancelDelay = 1.75,
+    CompleteDelay = 1.33,
+}
+
+local mainThread
+local equipThread
+
+-- ================= CORE =================
+function BlatantAPI.Start()
+    if BlatantAPI.Config.Active then return end
+    BlatantAPI.Config.Active = true
+
+    equipThread = task.spawn(function()
+        while BlatantAPI.Config.Active do
+            pcall(RE_Equip.FireServer, RE_Equip, 1)
+            task.wait(0.1)
+        end
+    end)
+
+    mainThread = task.spawn(function()
+        while BlatantAPI.Config.Active do
+            pcall(function()
+                RF_Cancel:InvokeServer()
+                RF_Charge:InvokeServer(math.huge)
+                RF_Start:InvokeServer(-139.6379699707, 0.99647927980797)
+            end)
+
+            task.delay(BlatantAPI.Config.CompleteDelay, function()
+                if BlatantAPI.Config.Active then
+                    pcall(RE_Complete.FireServer, RE_Complete)
+                end
+            end)
+
+            task.wait(BlatantAPI.Config.CancelDelay)
+        end
+    end)
+end
+
+function BlatantAPI.Stop()
+    BlatantAPI.Config.Active = false
+    if mainThread then task.cancel(mainThread) end
+    if equipThread then task.cancel(equipThread) end
+    mainThread, equipThread = nil, nil
+    pcall(function() RF_Cancel:InvokeServer() end)
+end
+
+function BlatantAPI.SetMode(v)
+    BlatantAPI.Config.Mode = v
+end
+
+function BlatantAPI.SetDelay(cancel, complete)
+    if cancel then BlatantAPI.Config.CancelDelay = cancel end
+    if complete then BlatantAPI.Config.CompleteDelay = complete end
+end
+
+return BlatantAPI

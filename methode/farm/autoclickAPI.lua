135362cef7065dc310184e1bb44c312c05d60bdb
NYXOXOV3 <fortressjr25@gmail.com>
2026-02-23 02:58:12 +0700

    Update autoclickAPI.lua

--- methode/farm/autoclickAPI.lua
+++ methode/farm/autoclickAPI.lua
@@ -1,74 +1,66 @@
 -- =========================================================
--- AUTO FISHING CLICKED FUNCTION (NORMAL INSTANT)
+-- AUTO FISHING LEGIT FUNCTION
 -- =========================================================
 
-local AutoClickAPI = {}
+local AutoAPI = {}
 
 -- ================= SERVICES =================
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+-- ================= CONTROLLERS =================
+task.wait(0.2)
+local FishingController = require(
+    ReplicatedStorage:WaitForChild("Controllers").FishingController
+)
+
 -- ================= REMOTES =================
 local Net = ReplicatedStorage
-    :WaitForChild("Packages")
-    :WaitForChild("_Index")
-    :WaitForChild("sleitnick_net@0.2.0")
-    :WaitForChild("net")
+    .Packages._Index["sleitnick_net@0.2.0"].net
 
-local RF_Charge = Net["RF/FlgK:h oAkCC= D6"]
-local RF_Start = Net["RF/UiwN8vNL7vB>D5\";pA5;A7$B3Jx8>"]
---local RE_Done   = Net["RE/FishingCompleted"]
-local RF_Done   = Net["RF/Fez<;ICy6FIBF::Fg<"]
-local RF_Cancel = Net["RF/Fet<8o oAkCC=vCBwLA"]
-local RE_Equip  = Net["RE/EquipToolFromHotbar"]
+local RE_Equip = Net["RE/EquipToolFromHotbar"]
+local RF_Update = Net["RF/UpdateAutoFishingState"]
 
 -- ================= STATE =================
-AutoClickAPI.Enabled = false
-AutoClickAPI.Delay = 1.50
-local loopThread
+AutoAPI.Active = false
+AutoAPI.ClickSpeed = 0.05
+local clickThread
 local equipThread
 
 -- ================= CORE =================
-function AutoClickAPI.Start()
-    if AutoClickAPI.Enabled then return end
-    AutoClickAPI.Enabled = true
+function AutoAPI.Start()
+    if AutoAPI.Active then return end
+    AutoAPI.Active = true
 
-    loopThread = task.spawn(function()
-        while AutoClickAPI.Enabled do
-            local ts = os.time() + os.clock()
+    pcall(function()
+        RE_Equip:FireServer(1)
+        RF_Update:InvokeServer(true)
+    end)
 
-            pcall(function()
-                RF_Charge:InvokeServer(ts)
-                RF_Start:InvokeServer(-139.630452165, 0.99647927980797)
-            end)
-
-            task.wait(AutoClickAPI.Delay)
-
-            pcall(function() RF_Done:InvokeServer() end)
-            task.wait(0.3)
-            pcall(function() RF_Cancel:InvokeServer() end)
-
-            task.wait(0.1)
+    clickThread = task.spawn(function()
+        while AutoAPI.Active do
+            FishingController:RequestFishingMinigameClick()
+            task.wait(AutoAPI.ClickSpeed)
         end
     end)
 
     equipThread = task.spawn(function()
-        while AutoClickAPI.Enabled do
+        while AutoAPI.Active do
             pcall(function() RE_Equip:FireServer(1) end)
             task.wait(0.1)
         end
     end)
 end
 
-function AutoClickAPI.Stop()
-    AutoClickAPI.Enabled = false
-    if loopThread then task.cancel(loopThread) end
+function AutoAPI.Stop()
+    AutoAPI.Active = false
+    if clickThread then task.cancel(clickThread) end
     if equipThread then task.cancel(equipThread) end
-    loopThread, equipThread = nil, nil
+    clickThread, equipThread = nil, nil
 end
 
-function AutoClickAPI.SetDelay(v)
-    AutoClickAPI.Delay = v
+function AutoAPI.SetSpeed(v)
+    AutoAPI.ClickSpeed = v
 end
 
-return AutoClickAPI
+return AutoAPI
